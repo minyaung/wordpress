e0859f1752f8c618b476dbcf06822a29681874a2
Min Yaung <yaungmin@gmail.com>
2021-07-14 19:55:37 -0700

    Added necessary config files.

---
+++ config-localhost.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'username');
+define('DB_PASSWORD', 'password');
+define('DB_HOST', 'localhost');
+define('DB_COLLATE', 'utf8_general_ci');
+define('WP_CONTENT_DIR', '/usr/share/wordpress/wp-content');
+?>
